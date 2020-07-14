0de8c981be5ef6536c647ca45ee938714dcdb930
Aanu Babajide <anubabajide@gmail.com>
2020-07-15 00:58:16 +0100

    Created Models and Serializers

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Product(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    price = models.FloatField()
+    description = models.TextField(max_length=300)
+    image_url = models.URLField()
+    location = models.TextField(max_length=80)
+
+class Interest(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.TextField(max_length=80)
+    email = models.EmailField()
+    location = models.TextField(max_length=80)
+
+    class Meta:
+        unique_together = (('product', 'email'),)
---
+++ catalog/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+from .models import Product, Interest
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+        extra_kwargs = {'url':{'required': True}}
+
+class InterestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Interest
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {'password': {'required': True, 'write_only': True}}
+
+        def create(self, validated_data):
+            user = User.objects.create_user(**validated_data)
+            Token.objects.create(user=user)
+            return user
