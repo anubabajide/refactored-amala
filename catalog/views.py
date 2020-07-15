d09997e846c2a7cf621601503f2ef7f606ff24c6
Aanu Babajide <anubabajide@gmail.com>
2020-07-15 01:11:35 +0100

    Added views for Product and Interest

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from .models import Product, Interest
+from .serializers import ProductSerializer, InterestSerializer
 
 # Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = (IsAuthenticated,)
+
+
+class InterestViewSet(viewsets.ModelViewSet):
+    queryset = Interest.objects.all()
+    serializer_class = InterestSerializer
+    permission_classes = (AllowAny,)
+
+    def delete(self, request, *args, *kwargs):
+        response = {'message': 'Cannot delete Interest'}
+        return Response(response, status = status.HTTP_400_BAD_REQUEST)
